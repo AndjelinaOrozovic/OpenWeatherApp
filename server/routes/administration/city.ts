5cd347307f9c2523ef94acec2e4e0ef4cd6b6c60
Andjelina Orozovic <andjelina.orozovic@gmail.com>
2021-10-20 15:23:08 +0200

    changes commited

--- server/routes/administration/city.ts
+++ server/routes/administration/city.ts
@@ -17,32 +17,13 @@ export class CityRouter extends Router {
     this.router.route('/')
       .get(this.queryAll.bind(this))
       .post(this.createNewCity.bind(this)); // mozemo razdvojiti u novu rutu '/newcity' ali ovdje post ne smeta get-u, pa smo ih stavili zajedno
-    this.router.route('/search')
-      .get(this.getCity.bind(this))
-  }
-
-  async getCity(request: IRequest, response: IResponse, next: NextFunction) 
-  {
-    try {
-    const cityName = request.query.newCity;
-    const cityData = new Cities();
-    const oneCity = await cityData.getOneCity(<string>cityName);
-    // if(!oneCity) { 
-    //   response.data = [];
-    // } else response.data = [oneCity];
-    response.data = oneCity ? [oneCity] : [];
-    console.log(response.data);
-    next();
-    } catch (error) {
-      console.log(error);
-    }
+    this.router.route('/search')  //dodajemo novu rutu, za search
+      .get(this.getCity.bind(this));
   }
 
   async queryAll(request: IRequest, response: IResponse, next: NextFunction) {
     try {
-
       const cr = new CityRepository(this.server);
-
       response.data = await cr.query();
       next();
     } catch (error) {
@@ -58,32 +39,43 @@ export class CityRouter extends Router {
 
       //metoda za upis samo imena grada u bazu
       // const newCity = await cr.create((cityNew) => {
-        
       //   cityNew.name = city.name;
-
       // });
-      const cityData = new Cities();
-      const newCity = await cityData.getOneCity(city.name);
-      await cr.create((city) => {
-              city.id = newCity.id;
-              city.name = newCity.name;
-              city.coord = newCity.coord;
-              city.main = newCity.main;
-              city.dt = newCity.dt;
-              city.wind = newCity.wind;
-              city.sys = newCity.sys;
+
+      const newCity =  await cr.create((newCity) => {
+              newCity.id = city.id;
+              newCity.name = city.name;
+              newCity.coord = city.coord;
+              newCity.main = city.main;
+              newCity.dt = city.dt;
+              newCity.wind = city.wind;
+              newCity.sys = city.sys;
               //city.rain = newCity.rain;
               //city.snow = newCity.snow;
-              city.clouds = newCity.clouds;
-              city.weather = newCity.weather[0];
-              console.log(city);
+              newCity.clouds = city.clouds;
+              newCity.weather = city.weather[0];
             });
-            console.log(newCity);
 
-      // response.data = await cr.getOne({_id : newCity._id});
+      response.data = await cr.getOne({_id : newCity._id});
       next();
     } catch (error) {
       next(Router.handleError(error, request, response));
+    }
+  }
+
+  async getCity(request: IRequest, response: IResponse, next: NextFunction) 
+  {
+    try {
+    const cityName = request.query.newCity;
+    const cityData = new Cities();
+    const oneCity = await cityData.getOneCity(<string>cityName);
+    // if(!oneCity) { 
+    //   response.data = [];
+    // } else response.data = [oneCity];
+    response.data = oneCity ? [oneCity] : [];
+    next();
+    } catch (error) {
+      console.log(error);
     }
   }
 
